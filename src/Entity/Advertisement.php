f08010388bafb4919139bffbf8d5622133d9c37d
Victor <92865037+CN-Works@users.noreply.github.com>
2025-10-07 13:52:28 +0200

    Chore : creating advertisement & company entity

---
+++ src/Entity/Advertisement.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdvertisementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdvertisementRepository::class)]
+class Advertisement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $salary = null;
+
+    #[ORM\Column]
+    private ?int $workhours = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSalary(): ?int
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(int $salary): static
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+
+    public function getWorkhours(): ?int
+    {
+        return $this->workhours;
+    }
+
+    public function setWorkhours(int $workhours): static
+    {
+        $this->workhours = $workhours;
+
+        return $this;
+    }
+}
